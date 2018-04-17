19b37e67cb20269b95aa46f49c8a3b18ac3d313e
Aleksandr Nesterenko <ext.aanesterenko@tinkoff.ru>
2018-04-18 00:16:02 +0300

    lesson-1: tasks 3, 4

--- scripts/lesson1.js
+++ scripts/lesson1.js
@@ -1,5 +1,5 @@
 //1. реализовать функцию которая вернет сумму элементов произвольного массива
-function arraySum(array) {
+function getArraySum1(array) {
     let sum = 0;
 
     for (let i = 0; i < array.length; i++) {
@@ -9,14 +9,28 @@ function arraySum(array) {
     return sum;
 }
 
+function getArraySum2(array) {
+    let sum = 0;
+
+    array.forEach((item) => sum += item);
+
+    return sum;
+}
+
+function getArraySum3(array) {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+    return array.reduce(reducer);
+}
+
 //2. реализовать функцию которая принимает произвольный массив и возвращает отсортированный по возростанию, сделать
 // двумя способами, через метод sort и через цикл for
 
-function arraySort(array) {
-    return array.sort(function(a, b){return a - b});
+function getSortedArray1(array) {
+    return array.sort((a, b) => {return a - b});
 }
 
-function arrayLoopSort(array) {
+function getSortedArray2(array) {
     for (let i = 0; i < array.length; i++) {
         for (let j = 0; j < array.length; j++) {
             if (array[i] < array[j]) {
@@ -30,7 +44,25 @@ function arrayLoopSort(array) {
 }
 
 //3. реализовать функцию которая возвращает массив с уникальными элементами // [1,1,2,3,1,2] => [1,2,3]
+function getUniqueArray(array) {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if (array[i] === array[j] && i !== j) {
+                array.splice(j, 1);
+            }
+        }
+    }
+    return getSortedArray1(array);
+}
 
 //4. реализовать функцию которая возвращает массив с числами фибоначчи, количество которых зависит
 // от переданного значения при вызове функции
+function getFibonacci(count) {
+    let array = [0, 1];
 
+    for (let i = 0; i < count; i++) {
+        array[i + 2] = array[i] + array[i + 1];
+    }
+
+    return array;
+}
